25b8d439b7ff035ed18732e39a9425eb2f2f4340
matthewpdias <matthewpdias@gmail.com>
2017-07-25 09:38:26 -0700

    added timestamp output to generate_traffic.sh

--- generate_traffic.sh
+++ generate_traffic.sh
@@ -2,6 +2,10 @@
 
 source platform.secrets.sh
 
+timestamp() {
+  date +"%T"
+}
+
 jobs=(job/sample_pipelines/job/joda-time job/sample_pipelines/job/game-of-life job/sample_pipelines/job/libotrio job/sample_pipelines/job/spring-petclinic)
 MINWAIT=2
 MAXWAIT=5
@@ -9,7 +13,7 @@ MAXWAIT=5
 while [ 1 ]; do
   nextjob=${jobs[$RANDOM % ${#jobs[@]}]} # Random next job
   waittime=$[$RANDOM % ($MAXWAIT - $MINWAIT) + $MINWAIT] # Random wait time
-  echo "Triggering $nextjob"
+  echo "Triggering $nextjob @ $(timestamp)"
   docker exec jenkins curl -s -X POST jenkins:${PASSWORD_JENKINS}@localhost:8080/jenkins/$nextjob/build
   echo "Waiting $waittime minutes."
   sleep $(($waittime*60))
